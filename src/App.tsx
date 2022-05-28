bd12efc23866d28f590b4a648a4c24e78a8c417f
Elmer Almeida <almeielm@sheridancollege.ca>
2022-05-28 12:32:33 -0400

    minor formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import Footer from './components/Footer';
 import './App.css';
-import Game from "./Game";
+import Game from './Game';
 
 const App: React.FC = () => {
-    return (
-        <div className='App'>
-            <div className='content'>
-                <h1 className='headline'>Memory Game</h1>
-                <p className="about">A simple Pokemon<sup><small>&copy;</small></sup> game application made with React
-                    and Typescript.</p>
-                <Game/>
-            </div>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <div className='content'>
+        <h1 className='headline'>Memory Game</h1>
+        <p className='about'>
+          A simple Pokemon
+          <sup>
+            <small>&copy;</small>
+          </sup>{' '}
+          game application made with React and Typescript.
+        </p>
+        <Game />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
