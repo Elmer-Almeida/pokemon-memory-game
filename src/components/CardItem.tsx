841b894c2769bcd9c7334e9ddc1cd98986d311ac
Elmer Almeida <almeielm@sheridancollege.ca>
2022-05-28 21:34:24 -0400

    minor formatting

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,40 +1,32 @@
 import React from 'react';
-import {Card} from '../models/Card';
+import { Card } from '../models/Card';
 import card_back from '../images/card_back.jpeg';
 
 interface Props {
-    card: Card;
-    flipped: boolean;
-    disabled: boolean;
-    handleSelection: (card: Card) => void;
+  card: Card;
+  flipped: boolean;
+  disabled: boolean;
+  handleSelection: (card: Card) => void;
 }
 
 const CardItem: React.FC<Props> = ({
-                                       card,
-                                       flipped,
-                                       disabled,
-                                       handleSelection,
-                                   }) => {
-    const handleCardSelection = (card: Card) => {
-        if (!disabled) handleSelection(card);
-    };
+  card,
+  flipped,
+  disabled,
+  handleSelection,
+}) => {
+  const handleCardSelection = (card: Card) => {
+    if (!disabled) handleSelection(card);
+  };
 
-    return (
-        <div className='card' onClick={() => handleCardSelection(card)}>
-            <div className={flipped ? 'card__flipped' : ''}>
-                <img
-                    src={card.src}
-                    alt={card.src}
-                    className='card__front'
-                />
-                <img
-                    src={card_back}
-                    alt={card_back}
-                    className='card__back'
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className='card' onClick={() => handleCardSelection(card)}>
+      <div className={flipped ? 'card__flipped' : ''}>
+        <img src={card.src} alt={card.src} className='card__front' />
+        <img src={card_back} alt={card_back} className='card__back' />
+      </div>
+    </div>
+  );
 };
 
 export default CardItem;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,50 @@
-import {IoMailOutline} from "react-icons/io5";
-import {FaGithub, FaLinkedinIn, FaTwitter} from "react-icons/fa";
+import { IoMailOutline } from 'react-icons/io5';
+import { FaGithub, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
-    return (
-        <footer>
-            <p className="icons">
-                <a href="mailto:almeielm@sheridancollege.ca?subject=Pokemon Memory Game Inquiry" title={"Send E-Mail"}
-                   target={"_blank"}>
-                    <IoMailOutline/>
-                </a>
-                <a href="https://www.twitter.com/_elmeralmeida" title={"Elmer's Twitter Profile"} target={"_blank"}>
-                    <FaTwitter/>
-                </a>
-                <a href="https://www.github.com/Elmer-Almeida" title={"Elmer's GitHub Profile"} target={"_blank"}>
-                    <FaGithub/>
-                </a>
-                <a href="https://www.linkedin.com/in/elmeralmeida/" title={"Elmer's LinkedIn Profile"}
-                   target={"_blank"}>
-                    <FaLinkedinIn/>
-                </a>
-            </p>
-            <p className={"createdby"}>
-                Created by Elmer Almeida
-            </p>
-            <p className={"contact"}>
-                <a href='mailto:almeielm@sheridancollege.ca?subject=Pokemon Memory Game Inquiry'
-                   title='Send me a message'>
-                    almeielm@sheridancollege.ca
-                </a>
-            </p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p className='icons'>
+        <a
+          href='mailto:almeielm@sheridancollege.ca?subject=Pokemon Memory Game Inquiry'
+          title={'Send E-Mail'}
+          target={'_blank'}
+        >
+          <IoMailOutline />
+        </a>
+        <a
+          href='https://www.twitter.com/_elmeralmeida'
+          title={"Elmer's Twitter Profile"}
+          target={'_blank'}
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href='https://www.github.com/Elmer-Almeida'
+          title={"Elmer's GitHub Profile"}
+          target={'_blank'}
+        >
+          <FaGithub />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/elmeralmeida/'
+          title={"Elmer's LinkedIn Profile"}
+          target={'_blank'}
+        >
+          <FaLinkedinIn />
+        </a>
+      </p>
+      <p className={'createdby'}>Created by Elmer Almeida</p>
+      <p className={'contact'}>
+        <a
+          href='mailto:almeielm@sheridancollege.ca?subject=Pokemon Memory Game Inquiry'
+          title='Send me a message'
+        >
+          almeielm@sheridancollege.ca
+        </a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
